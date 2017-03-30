6a97293e2fa3f744d331c3ff2d8880e27eddd09d
FezZHD <zhenya535@gmail.com>
2017-03-31 02:22:20 +0300

    trying to connect to db

--- src/main/java/com/bsuir/mpp/travelagency/controllers/HelloSpring.java
+++ src/main/java/com/bsuir/mpp/travelagency/controllers/HelloSpring.java
@@ -4,6 +4,7 @@ package com.bsuir.mpp.travelagency.controllers;
  * Created by zheny on 3/26/2017.
  */
 
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
